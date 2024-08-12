d023cf9ffa75d41d41f54979f1bf0ac011468c59
balak26 <bamlakgezahegn26@gmail.com>
2024-08-12 19:12:33 +0300

    form, style, and mock js code

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("connected");
+const myLibrary = [];
+
+function Book(title, author, page, isRead) {
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.isRead = isRead;
+}
+
+function addBook() {
+  console.log("submitted");
+}
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addBook();
+});
