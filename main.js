3cee8e54ad9263dff2544f8f7cd87ffdbac1c745
balak26 <bamlakgezahegn26@gmail.com>
2024-08-13 22:24:08 +0300

    function added to render array in the html continously to, a delete button and fucntion to remove books form the array and html

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const con = document.querySelector("#con");
 
 console.log(myLibrary);
 
-myLibrary.filter((item) => item.title.includes("f"));
-
 function Book(title, author, page, isRead) {
   this.title = title;
   this.author = author;
@@ -13,7 +11,29 @@ function Book(title, author, page, isRead) {
   this.isRead = isRead;
 }
 
-// function addBook(title) {}
+const book1 = new Book("dad", "mom", 33, "read");
+const book2 = new Book("mom", "dad", 111, "reading");
+const book3 = new Book("last", "dad", 111, "reading");
+
+myLibrary.push(book1, book2, book3);
+
+function render() {
+  const html = myLibrary
+    .map(
+      (book, index) =>
+        `<ul class="table"><li>${book.title}</li><li>${book.author}</li><li>${book.page}</li><li>${book.isRead}</li><button onclick="delBook(${index})" id="del">Delete</button></ul>`
+    )
+    .join("");
+
+  con.innerHTML = html;
+}
+
+render();
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  render();
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,5 +43,19 @@ form.addEventListener("submit", (e) => {
   const { title, page, author, isRead } = toObject;
   const book = new Book(title, author, page, isRead);
 
-  myLibrary.push(book);
+  addBookToLibrary(book);
 });
+
+const del = document.querySelectorAll("#del");
+
+function delBook(index) {
+  myLibrary.splice(index, 1);
+  render();
+}
+
+// del.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const index = e.target.index;
+//     delBook(index);
+//   });
+// });
